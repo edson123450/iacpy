5b96dc488903f5d51ba592add5d1b08fcab9219c
edson123450 <142638227+edson123450@users.noreply.github.com>
2024-09-15 21:59:10 -0500

    Update iac.py

--- iac.py
+++ iac.py
@@ -1,45 +1,39 @@
 from aws_cdk import (
     aws_ec2 as ec2,
-    core
+    App, Stack, Environment
 )
 
-
-class MyEc2Stack(core.Stack):
-
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+class MyEc2Stack(Stack):
+    def __init__(self, scope: App, id: str, **kwargs):
         super().__init__(scope, id, **kwargs)
+
 
         vpc = ec2.Vpc(self, "MyVpc", max_azs=2)
 
-        security_group = ec2.SecurityGroup(self, "InstanceSecurityGroup",
-                                           vpc=vpc,
-                                           security_group_name="AllowSSH_HTTP",
-                                           description="Permitir tráfico SSH y HTTP desde cualquier lugar",
-                                           allow_all_outbound=True
-                                           )
 
-        # Permitir tráfico SSH y HTTP
-        security_group.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(22), "Permitir SSH desde cualquier lugar")
-        security_group.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(80), "Permitir HTTP desde cualquier lugar")
-
-        # Configurar la instancia EC2
-        instance = ec2.Instance(self, "MyInstance",
-                                instance_type=ec2.InstanceType("t2.micro"),
-                                machine_image=ec2.MachineImage.generic_linux({"us-east-1": "ami-049d6539112c3246e"}),
-                                vpc=vpc,
-                                key_name="vockey",
-                                security_group=security_group
-                                )
-
-        instance.user_data.add_commands(
-            "sudo yum update -y",
-            "sudo yum install -y httpd",
-            "sudo systemctl start httpd",
-            "sudo systemctl enable httpd",
-            "cd /var/www/html",
-            "git clone https://github.com/edson123450/websimple.git",
-            "git clone https://github.com/edson123450/webplantilla.git"
+        security_group = ec2.SecurityGroup(
+            self,
+            "InstanceSecurityGroup",
+            vpc=vpc,
+            allow_all_outbound=True,
+            description="Permitir tráfico SSH y HTTP desde cualquier lugar"
         )
-app = core.App()
-MyEc2Stack(app, "MyEc2Stack")
+        security_group.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(22), "Permitir SSH")
+        security_group.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(80), "Permitir HTTP")
+
+
+        ec2.Instance(
+            self,
+            "MyInstance",
+            instance_type=ec2.InstanceType("t2.micro"),
+            machine_image=ec2.MachineImage.generic_linux({"us-east-1": "ami-049d6539112c3246e"}),
+            vpc=vpc,
+            key_name="vockey",
+            security_group=security_group
+        )
+
+
+
+app = App()
+MyEc2Stack(app, "MyEc2Stack", env=Environment(account="522813847784", region="us-east-1"))
 app.synth()
